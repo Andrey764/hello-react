08bf1b541098592955a2f9f59921fb24900d66c1
Andrey764 <=>
2021-10-15 12:47:12 +0300

    git stztus

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { User } from "./User";
+import  './App.css';
+import { Citi } from "./Citi";
 function App() {
+  let arr =["..Images/1543919169.jpg",
+  "../Images/unnamed.jpg",
+  "../Images/unnamed (1).jpg"]
+  let user = new User("Григорьев","Андрей","Викторович","0684267002","Email@.com");
+  let citi = new Citi("Кривой Рог","Украина","24 августа 1991 г", arr)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="Text">Hello, React</h2>
+      <div className="Conteiner">
+      {user.getUser()}
+      <div>
+        {citi.getSiti()}
+        {citi.getPaths()}
+      </div>
+      </div>
     </div>
   );
 }
